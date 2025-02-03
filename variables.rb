9bce8ed99308416ae1c26be3a07c1a257c7f6eda
vcosmin2701 <v.cosmin2717@gmail.com>
2025-02-03 22:26:59 +0200

    Basic introduction to variables

---
+++ variables.rb
@@ -0,0 +1,20 @@
+age = 32
+multiplier = 10
+
+total = age * multiplier
+
+puts total
+
+# types of variables
+# global variable -> $apple
+# instance variable -> @apple
+# class variable -> @@apple
+# constant -> APPLE
+
+# practice exercise
+
+orange = 300
+apple = 120
+
+total = orange * apple
+puts total
